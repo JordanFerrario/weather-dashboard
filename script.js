bcbfabe982ed916b8c3f24af4930ae069921b42b
Jordan Ferrario <108853449+JordanFerrario@users.noreply.github.com>
2024-03-23 14:35:50 -0700

    created api calls to OpenWeather. created functionality that takes in a city search by name, fetches the coordinates to that city, and then fetches both the current weather data and the 5-day forecast.

--- script.js
+++ script.js
@@ -2,8 +2,85 @@ const sidebar = document.getElementById("sidebar");
 let touchStartX = 0;
 let touchEndX = 0;
 let lastScrollY = window.scrollY;
+const weatherApiKey = "55e089ff3707c1a7d257809d2f463710";
+let lat;
+let lon;
+let citySearchInput;
+const units = "imperial";
 
-// OpenWeather
+const citySearch = document.getElementById("city-search");
+const searchBtn = document.getElementById("search-btn");
+
+// Get OpenWeather data
+function getFiveDayWeatherData(lat, lon) {
+  const fiveDayWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=${units}`;
+
+  fetch(fiveDayWeatherURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data) {
+        // render to UI
+        console.log("5 day weather:", data);
+      }
+    });
+}
+
+function getCurrentWeatherData(lat, lon) {
+  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=${units}`;
+
+  fetch(currentWeatherURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data) {
+        // render to UI
+        console.log("current weather:", data);
+      }
+    });
+}
+
+// convert city name to lat and lon
+function getCoordinatesFromCitySearchInput() {
+  citySearchInput = citySearch.value;
+
+  if (citySearchInput) {
+    const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearchInput}&appid=${weatherApiKey}`;
+
+    // pass city name to geocoordinates api
+    fetch(geocodingURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw response.json();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data && data[0].lat && data[0].lon) {
+          lat = data[0].lat;
+          lon = data[0].lon;
+          getCurrentWeatherData(lat, lon);
+          getFiveDayWeatherData(lat, lon);
+        } else {
+          console.log("No city name found in the response");
+        }
+      });
+  } else {
+    console.log("Search field is empty");
+  }
+}
+
+// render weather data to UI
+
+searchBtn.addEventListener("click", getCoordinatesFromCitySearchInput);
 
 // navbar scrolling
 // window.addEventListener("scroll", () => {
